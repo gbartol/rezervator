937ea911c2c596cb0a7f7c70caf5409bd6582f44
RockHam <m2.njegac@gmail.com>
2025-12-11 10:46:09 +0100

    Creating and seeding databases
    Written the seeding functions:
    
    - seed_table_halls()
    - seed_table_locations()
    - seed_table_seats()
    
    Removed unnecessary table creating and seeding functions.

--- db.py
+++ db.py
@@ -113,9 +113,6 @@ def create_table_reservations():
 
 
 def create_table_plays():
-    # Stvori tablicu quacks.
-    # Svaki quack ima svoj id (automatski će se povećati za svaku novoubačeni quack), id korisnika koji je objavio quack,
-    # samu poruku koja čini quack, te datum i vrijeme kad je quack objavljen.
     db = get_db_connection();
     cursor = db.cursor();
 
@@ -134,9 +131,6 @@ def create_table_plays():
 
 
 def create_table_performances():
-    # Stvori tablicu quacks.
-    # Svaki quack ima svoj id (automatski će se povećati za svaku novoubačeni quack), id korisnika koji je objavio quack,
-    # samu poruku koja čini quack, te datum i vrijeme kad je quack objavljen.
     db = get_db_connection();
     cursor = db.cursor();
 
@@ -155,9 +149,6 @@ def create_table_performances():
 
 
 def create_table_halls():
-    # Stvori tablicu quacks.
-    # Svaki quack ima svoj id (automatski će se povećati za svaku novoubačeni quack), id korisnika koji je objavio quack,
-    # samu poruku koja čini quack, te datum i vrijeme kad je quack objavljen.
     db = get_db_connection();
     cursor = db.cursor();
 
@@ -174,9 +165,6 @@ def create_table_halls():
 
 
 def create_table_locations():
-    # Stvori tablicu quacks.
-    # Svaki quack ima svoj id (automatski će se povećati za svaku novoubačeni quack), id korisnika koji je objavio quack,
-    # samu poruku koja čini quack, te datum i vrijeme kad je quack objavljen.
     db = get_db_connection();
     cursor = db.cursor();
 
@@ -193,9 +181,6 @@ def create_table_locations():
 
 
 def create_table_seats():
-    # Stvori tablicu quacks.
-    # Svaki quack ima svoj id (automatski će se povećati za svaku novoubačeni quack), id korisnika koji je objavio quack,
-    # samu poruku koja čini quack, te datum i vrijeme kad je quack objavljen.
     db = get_db_connection();
     cursor = db.cursor();
 
@@ -212,7 +197,6 @@ def create_table_seats():
     return f'create_table_seats: OK';
 
 
-# -------------------- TODO: Zamijeniti ovo sa stvarnim tablicama koje ćemo koristiti
 def seed_table_users():
     # Uočimo da ne treba specificirati id koji se automatski poveća kod svakog ubacivanja.
     db = get_db_connection();
@@ -294,28 +278,22 @@ def seed_table_performances():
     db = get_db_connection();
     cursor = db.cursor();
     
-    # 'id_performance int NOT NULL PRIMARY KEY AUTO_INCREMENT,' +
-    #     'id_play int NOT NULL FOREIGN KEY,' +
-    #     'id_hall int NOT NULL FOREIGN KEY,' +
-    #     'date_performance DATETIME NOT NULL,' +
-    #     'price_performance float(10))' 
-    #sql = 'INSERT INTO rezervator_reservations(id_user, id_performance, id_seat) VALUES (%(id_user)s, %(id_performance)s, %(id_seat)s)'
     sql = 'INSERT INTO rezervator_performances(id_play, id_hall, date_performance, price_performance) VALUES (%(id_play)s, %(id_hall)s, %(date)s, %(price)s)'
 
     cursor execute (sql ,
-        {'id_play' : '1', 'id_hall' : '1', 'date' : '2026-02-07 17:50:00', 'price' : '10' }) 
+        {'id_play' : '1', 'id_hall' : '1' , 'date' : '2026-02-07 17:50:00', 'price' : '10' }) 
 
     cursor execute (sql ,
-        {'id_play' : '1', 'id_hall' : '2', 'date' : '2026-02-07 19:00:00', 'price' : '10' }) 
+        {'id_play' : '1', 'id_hall' : '2',  'date' : '2026-02-07 19:00:00', 'price' : '10' }) 
 
     cursor execute (sql ,
-        {'id_play' : '2', 'id_hall' : '1', 'date' : '2026-01-24 17:00:00', 'price' : '7' }) 
+        {'id_play' : '2', 'id_hall' : '1',  'date' : '2026-01-24 17:00:00', 'price' : '7' }) 
 
     cursor execute (sql ,
-        {'id_play' : '2', 'id_hall' : '2', 'date' : '2026-01-24 17:45:00', 'price' : '7' })
+        {'id_play' : '2', 'id_hall' : '2',  'date' : '2026-01-24 17:45:00', 'price' : '7' })
 
     cursor execute (sql ,
-        {'id_play' : '3', 'id_hall' : '1', 'date' : '2026-02-06 17:00:00', 'price' : '8' }) 
+        {'id_play' : '3', 'id_hall' : '1',  'date' : '2026-02-06 17:00:00', 'price' : '8' }) 
 
     cursor execute (sql ,
         {'id_play' : '3', 'id_hall' : '1', 'date' : '2026-02-06 17:45:00', 'price' : '8' }) 
@@ -331,7 +309,13 @@ def seed_table_halls():
     db = get_db_connection();
     cursor = db.cursor();
 
-    
+    sql = 'INSERT INTO rezervator_halls(id_hall, id_location, hall_capacity) VALUES (%(id_hall)s, %(id_location)s, %(capacity)s)'
+
+    cursor.execute(sql, 
+    {'id_hall' : '1' , 'capacity' : '100'})
+
+    cursor.execute(sql, 
+    {'id_hall' : '2' , 'capacity' : '80'})
 
     db.commit();
     cursor.close();
@@ -343,7 +327,11 @@ def seed_table_locations():
     db = get_db_connection();
     cursor = db.cursor();
 
-    
+
+    sql = 'INSERT INTO rezervator.locations(name_location, address_location ) VALUES (%(name)s, %(address)s)' 
+
+    cursor.execute(sql ,
+    {'name' : 'Samobor', 'address' : 'Ulica Mate Lovraka 4'})
 
     db.commit();
     cursor.close();
@@ -355,6 +343,15 @@ def seed_table_seats():
     db = get_db_connection();
     cursor = db.cursor();
 
+    sql = 'INSERT INTO rezervator.seats(id_hall, row_seat, column_seat) VALUES (%(id_hall)s, %(row)s, %(col)s)'
+
+    for i in range (1,11):
+        for j in range (1, 11):
+            cursor.execute(sql, {'id_hall' : '1', 'row' : i, 'col' : j})
+
+    for i in range (1,11):
+        for j in range (1, 9):
+            cursor.execute(sql, {'id_hall' : '2', 'row' : i, 'col' : j})
     
 
     db.commit();
@@ -362,74 +359,3 @@ def seed_table_seats():
 
     return f'seed_table_seats: OK';
 
-# -------------------- TODO: Zamijeniti ovo sa stvarnim tablicama koje ćemo koristiti
-def seed_table_follows():
-    # Ubaci neke followere unutra (ovo nije baš pametno ovako raditi, preko hardcodiranih id-eva usera)
-    db = get_db_connection();
-    cursor = db.cursor();
-
-    sql = 'INSERT INTO dz2_follows(id_user, id_followed_user) VALUES (%(id1)s, %(id2)s)';
-
-    cursor.execute( sql,
-        {'id1': 1, 'id2': 2} ); # elon -> KingJames
-
-    cursor.execute( sql,
-        {'id1': 2, 'id2': 1} ); # KingJames -> elon
-
-    cursor.execute( sql,
-        {'id1': 3, 'id2': 1} ); # StephenCurry30 -> elon
-
-    cursor.execute( sql,
-        {'id1': 1, 'id2': 4} ); # elon -> billgates
-
-    cursor.execute( sql,
-        {'id1': 4, 'id2': 1} ); # billgates -> elon
-
-    db.commit();
-    cursor.close();
-
-    return f'seed_table_follows: OK';
-
-
-# -------------------- TODO: Zamijeniti ovo sa stvarnim tablicama koje ćemo koristiti
-def seed_table_quacks():
-    # Ubaci neke quackove unutra (ovo nije baš pametno ovako raditi, preko hardcodiranih id-eva usera)
-    db = get_db_connection();
-    cursor = db.cursor();
-
-    sql = 'INSERT INTO dz2_quacks(id_user, quack, date) VALUES (%(id_user)s, %(quack)s, %(date)s)';
-
-    cursor.execute( sql,
-        {'id_user': 1, 'quack': 'Congrats to @KingJames for his record and to @StephenCurry30 for scoring 49 points! Big fan of both #Lakers and #Warriors!', 'date': '2025-11-17 19:45:00' } );
-
-    cursor.execute( sql,
-        {'id_user': 1, 'quack': 'Thank you for the kind words, @billgates. #quack is so much better indeed...', 'date': '2025-11-18 17:45:00' } );
-
-    cursor.execute( sql,
-        {'id_user': 2, 'quack': 'I am finally back in action in the #NBA! 23rd season!', 'date': '2025-11-17 17:45:00' } );
-
-    cursor.execute( sql,
-        {'id_user': 2, 'quack': 'Big win over #UtahJazz! I wish I could have played vs @StephenCurry30 and the #Warriors...', 'date': '2025-11-18 23:23:23' } );
-
-    cursor.execute( sql,
-        {'id_user': 3, 'quack': 'Congrats to @KingJames of the #Lakers for his record 23rd season! #seeyousoon', 'date': '2025-11-19 03:30:30' } );
-
-    cursor.execute( sql,
-        {'id_user': 3, 'quack': 'Practising some 3-pointers for the upcoming game vs #UtahJazz!', 'date': '2025-11-18 12:32:45' } );
-
-    cursor.execute( sql,
-        {'id_user': 4, 'quack': 'I am a big fan of @elon! Driving my #tesla every day! But I prefer #quack :)', 'date': '2025-11-17 23:55:00' } );
-
-    cursor.execute( sql,
-        {'id_user': 4, 'quack': 'Well done @StephenCurry30! Good luck vs #UtahJazz #NBA', 'date': '2025-11-18 22:10:00' } );
-
-    cursor.execute( sql,
-        {'id_user': 3, 'quack': 'I\'m very happy to use #quack! Sorry, @elon, but #quack is perfect!', 'date': '2025-11-17 12:00:05' } );
-
-    cursor.execute( sql,
-        {'id_user': 3, 'quack': 'Best day ever vs #Spurs! Almost #50points', 'date': '2025-11-16 12:00:00' } );
-
-    db.commit();
-    cursor.close();
-
-    return f'seed_table_quacks: OK';
